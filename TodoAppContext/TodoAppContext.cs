3d20b8b28008fe0d0d85e1b6a94f65ed96a50551
Ömer Kahveci <omerkahveci53@hotmail.com>
2023-10-26 11:34:53 +0300

    Add project files.

---
+++ TodoAppContext/TodoAppContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoAppModel;
+
+namespace TodoAppContext
+{
+    public partial class TodoAppContext:DbContext
+    {
+        public TodoAppContext()
+        {}
+        public TodoAppContext(DbContextOptions<TodoAppContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql();
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
